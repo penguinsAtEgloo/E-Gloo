60cfcf5268cb83374b208e34a5bea918633d21d8
haewonhan <hhan9243@gmail.com>
2021-07-06 20:41:15 +0900

    chore/FE/configuration

---
+++ front-end/layouts/default.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <main>
+      <Nuxt />
+    </main>
+  </div>
+</template>
+<style>
+:root {
+  --primary-color: #00c58e;
+}
+
+body {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+    Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji,
+    Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+  margin: 0;
+}
+
+a,
+a:visited {
+  text-decoration: none;
+  color: inherit;
+}
+
+a:hover,
+a.nuxt-link-exact-active {
+  color: var(--primary-color);
+}
+
+main {
+  margin: 0 auto;
+  margin-top: -10vh;
+  margin-bottom: 0 auto;
+  padding: 0 1rem;
+  max-width: 1280px;
+  text-align: center;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+li {
+  margin: 0 0.5rem;
+  padding: 0.25rem;
+  font-size: 1.2rem;
+}
+
+nav {
+  padding: 0 1rem;
+}
+</style>
---
+++ front-end/pages/register.vue
@@ -0,0 +1,128 @@
+<template>
+  <div>
+    <form @submit.prevent>
+      <label for="name">이름 *</label>
+      <input v-model="user.name" id="name" type="text" />
+      <label for="password">비밀번호 *</label>
+      <input v-model="user.password" id="password" type="text" />
+      <label for="password2">비밀번호 *</label>
+      <input v-model="user.password" id="password2" type="text" />
+      <label for="terms">약관 동의 *</label>
+      <b-container
+        id="terms"
+      >
+        <b-form-group
+          v-slot="{ ariaDescribedby }"
+        >
+          <b-form-checkbox
+            v-for="option in options"
+            v-model="selected"
+            :key="option.value"
+            :value="option.value"
+            :aria-describedby="ariaDescribedby"
+            size="sm"
+            stacked
+          >
+          {{ option.text }}
+          </b-form-checkbox>
+        </b-form-group>
+      </b-container>
+      <NuxtLink to="/register2" class="button">계속하기</NuxtLink>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'auth',
+  data() {
+    return {
+      user: {
+        name: '',
+        password: ''
+      },
+      selected: [], // Must be an array reference!
+      options: [
+        { text: 'Orange', value: 'orange' },
+        { text: 'Apple', value: 'apple' },
+        { text: 'Pineapple', value: 'pineapple' },
+        { text: 'Grape', value: 'grape' }
+      ]
+    }
+  }
+}
+</script>
+<style scoped>
+div {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+label {
+  font-size: 14px;
+  margin: 0 0.5rem;
+  margin-bottom: 0.25rem;
+  text-align: left;
+}
+
+input {
+  font-size: 16px;
+  padding: 0.75rem 1rem;
+  margin: 0 0.5rem;
+  margin-bottom: 0.5rem;
+  border: 1px solid #dedede;
+  border-radius: 0.5rem;
+  box-shadow: none;
+  box-sizing: border-box;
+}
+
+.button {
+  margin-top: 0.5rem;
+}
+
+.button[disabled] {
+  cursor: default;
+  opacity: 0.5;
+  cursor: not-allowed;
+}
+.button {
+  background: var(--primary-color);
+  padding: 0.75rem 1.5rem;
+  outline: none;
+  border-radius: 0.5rem;
+  font-size: 1rem;
+  border: none;
+  cursor: pointer;
+  color: white;
+  font-weight: 500;
+  text-transform: uppercase;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  letter-spacing: 0.5px;
+  margin: 0 0.5rem;
+}
+
+button:hover {
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  opacity: 0.8;
+}
+.custom-checkbox {
+  min-height: 0vh;
+  justify-content: left;
+}
+.container {
+  min-height: 0vh;
+  justify-content: left;
+  border-color: black;
+  border: 1px solid #dedede;
+  border-radius: 0.5rem;
+  padding: 0.75rem 0.75rem;
+  margin: 0 0.5rem;
+}
+</style>
