659da758152962fed4d895a3879469688984d51b
dongyeon94 <dongyeon94@naevr.com>
2021-07-13 00:27:57 +0900

    (#18) 로그인 기능개발

--- back-end-api/src/main/java/com/project/egloo/dto/response/SignUpResponse.java
+++ back-end-api/src/main/java/com/project/egloo/dto/response/SignUpResponse.java
@@ -1,4 +1,18 @@
 package com.project.egloo.dto.response;
 
+import java.util.HashMap;
+
 public class SignUpResponse {
+    private int code;
+
+    public SignUpResponse(int code) {
+        this.code = code;
+    }
+
+    public HashMap response(Object msg){
+        HashMap response = new HashMap();
+        response.put("code",this.code);
+        response.put("msg", msg);
+        return response;
+    }
 }
--- back-end-api/src/main/java/com/project/egloo/repository/MemberRespository.java
+++ back-end-api/src/main/java/com/project/egloo/repository/MemberRespository.java
@@ -4,7 +4,9 @@ import com.project.egloo.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface MemberRespository extends JpaRepository<Member,Long>{
     Member findByUserIdAndPassword(String userId, String password);
+    Member findByUserId(String userId);
 }
