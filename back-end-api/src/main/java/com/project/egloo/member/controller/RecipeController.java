5fb0a0d1d1011c8dc9193f8159838f8dd9160ee0
dy <dy@test>
2021-07-27 13:18:10 +0900

    (#31) 레시피 검색 기능

--- back-end-api/src/main/java/com/project/egloo/member/controller/RecipeController.java
+++ back-end-api/src/main/java/com/project/egloo/member/controller/RecipeController.java
@@ -1,11 +1,25 @@
 package com.project.egloo.member.controller;
 
+import com.project.egloo.member.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 @RestController
 @RequestMapping("/recipe")
 public class RecipeController {
 
+    @Autowired
+    private RecipeService recipeService;
+
+    @GetMapping("/getRecipeByIngredients")
+    public HashMap recipeByIngredients(@RequestParam ArrayList ingredient){
+        return recipeService.getRecipeByIngredients(ingredient);
+    }
 }
--- back-end-api/src/main/java/com/project/egloo/member/service/RecipeService.java
+++ back-end-api/src/main/java/com/project/egloo/member/service/RecipeService.java
@@ -1,7 +1,49 @@
 package com.project.egloo.member.service;
 
+import com.project.egloo.common.StatusCode;
+import com.project.egloo.member.domain.IngredientRecipeMapping;
+import com.project.egloo.member.repository.IngredientRecipeMappingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 public class RecipeService {
+
+    @Autowired
+    private IngredientRecipeMappingRepository ingredientRecipeMappingRepository;
+
+    public HashMap getRecipeByIngredients(ArrayList lis) {
+        ArrayList intersection = new ArrayList();
+        for(int i=0; i < lis.size(); i++){
+            List mappingList =  ingredientRecipeMappingRepository.findByIngredient_id(Long.parseLong((String) lis.get(i)));
+            List recipeNames = new ArrayList();
+            for(int j=0; j < mappingList.size(); j++){
+                IngredientRecipeMapping map = (IngredientRecipeMapping) mappingList.get(j);
+                recipeNames.add(map.getRecipe().getName());
+            }
+            intersection.add(recipeNames);
+        }
+        return  response(intersection(intersection));
+    }
+
+    public HashSet intersection(ArrayList inputArrays)
+    {
+        HashSet intersectionSet = new HashSet((Collection) inputArrays.get(0));
+        for (int i = 1; i < inputArrays.size(); i++)
+        {
+            HashSet innerset = new HashSet((Collection) inputArrays.get(i));
+            intersectionSet.retainAll(innerset);
+        }
+        return intersectionSet;
+    }
+
+    public HashMap response(Object object){
+        HashMap res = new HashMap();
+        res.put("code", StatusCode.SUCCESS_OK);
+        res.put("msg",object);
+        res.put("errors","");
+        return res;
+    }
 }
