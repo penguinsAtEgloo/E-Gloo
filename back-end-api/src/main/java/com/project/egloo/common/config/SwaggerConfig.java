16130256dd48ec97a82e94ea17627a59ceefdb5c
Park Sangwoo <tkddnsos@gmail.com>
2021-08-02 02:06:28 +0900

    (#71) swagger configuration

---
+++ back-end-api/src/main/java/com/project/egloo/common/config/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package com.project.egloo.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .consumes(getConsumeContentTypes())
+                .produces(getProduceContentTypes())
+                .apiInfo(this.apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.springframework.boot"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=UTF-8");
+        consumes.add("application/x-www-form-urlencoded");
+        return consumes;
+    }
+
+    private Set<String> getProduceContentTypes() {
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+        return produces;
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Egloo API Document")
+                .description("[Egloo] REST API")
+                .contact(new Contact("[Egloo Swagger]", "https://github.com/penguinsAtEgloo/E-Gloo", "helloegloo@gmail.com"))
+                .version("0.1.0")
+                .build();
+    }
+}
