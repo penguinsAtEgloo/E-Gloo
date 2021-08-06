88ef9c0a28f35bacd92593f914aa70c62e373561
Park Sangwoo <tkddnsos@gmail.com>
2021-08-06 21:05:19 +0900

    (#79) redis configuration

---
+++ back-end-api/src/main/java/com/project/egloo/common/config/AppConfig.java
@@ -0,0 +1,22 @@
+package com.project.egloo.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+class AppConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory("localhost", 6379);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}
