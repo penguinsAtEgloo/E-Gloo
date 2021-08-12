8424093b8b6a68a44e172dc8529381cfdfdc60bb
dongyeon94 <dongyeon94@naver.com>
2021-08-13 01:07:32 +0900

    (#87) 코드리뷰 사항 적용, consturctor injection 적용, code convention 적용

--- back-end-api/src/main/java/com/project/egloo/ingredient/repository/IngredientRepository.java
+++ back-end-api/src/main/java/com/project/egloo/ingredient/repository/IngredientRepository.java
@@ -4,9 +4,10 @@ import com.project.egloo.ingredient.domain.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
-    Optional<Ingredient> findByName(String name);
+    List<Ingredient> findByNameIn(ArrayList<String> ingredientList);
 }
