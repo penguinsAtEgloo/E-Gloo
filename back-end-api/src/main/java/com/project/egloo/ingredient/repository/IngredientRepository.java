dac4611d4b4061159d55841a64899e2376befce2
dongyeon94 <dongyeon94@naver.com>
2021-08-13 16:39:47 +0900

    2차 코드리뷰 @RequiredArgsConstructor 적용, Arraylist 타입 List타입으로 변경, 파라미터 및 return type 수정

--- back-end-api/src/main/java/com/project/egloo/ingredient/repository/IngredientRepository.java
+++ back-end-api/src/main/java/com/project/egloo/ingredient/repository/IngredientRepository.java
@@ -4,10 +4,9 @@ import com.project.egloo.ingredient.domain.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
-    List<Ingredient> findByNameIn(ArrayList<String> ingredientList);
+    List<Ingredient> findByNameIn(List<String> ingredients);
 }
