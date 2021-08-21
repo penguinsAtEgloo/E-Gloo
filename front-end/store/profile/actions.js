415a8b022111f73b7dda318479384dc697f9b82d
newtype94 <newtype94@korea.ac.kr>
2021-08-21 19:56:00 +0900

    data set 거치는 부분 수정
    추후 axios 통신 대비하여 mutations 에서 actions로 이동

--- front-end/store/profile/actions.js
+++ front-end/store/profile/actions.js
@@ -1,8 +1,28 @@
-export const firstDataFetch = ({ commit }, {userId}) => {
-  commit('getUserInfo', {userId});
-  commit('checkMyFollowing', {userId});
-  commit('getUserRecipes', {userId});
+import {users, recipes, myFollowings} from "~/assets/data/db"
+
+export const firstDataFetch = ({ dispatch }, {userId}) => {
+  dispatch('getUserInfo', {userId});
+  dispatch('checkMyFollowing', {userId});
+  dispatch('getUserRecipes', {userId});
 }
+
+export const getUserInfo = ({commit}, {userId}) => {
+  //userInfo get : axios.get()으로 변경
+  const selectedUser = users.filter((user) => user.userId==userId)[0];
+  commit('getUserInfo', {selectedUser});
+};
+
+export const checkMyFollowing = ({commit}, {userId}) => {
+  //나의 follow 여부 get : axios.get()으로 변경
+  const myFollowing = myFollowings.filter((myFollowing)=> myFollowing.userId==userId)[0].follow;
+  commit('checkMyFollowing', {myFollowing});
+};
+
+export const getUserRecipes = ({commit}, {userId}) => {
+  //userRecipes get : axios.get()으로 변경
+  const userRecipes = recipes.filter((recipe) => recipe.userId==userId);  
+  commit('getUserRecipes', {userRecipes});
+};
 
 export const updateMyFollowing = ({ commit }, {toFollow}) => {
   commit('updateMyFollowing', {toFollow});
--- front-end/store/profile/mutations.js
+++ front-end/store/profile/mutations.js
@@ -1,8 +1,4 @@
-import {users, recipes, myFollowings} from "~/assets/data/db"
-
-export const getUserInfo = (state, {userId}) => {
-  //userInfo get : axios.get()으로 변경
-  const selectedUser = users.filter((user) => user.userId==userId)[0];
+export const getUserInfo = (state, {selectedUser}) => {
   const {userImage, userIntro, follower, following} = selectedUser;
   state.userImage = userImage;
   state.userIntro = userIntro;
@@ -10,15 +6,11 @@ export const getUserInfo = (state, {userId}) => {
   state.following = following;
 };
 
-export const checkMyFollowing = (state, {userId}) => {
-  //나의 follow 여부 get : axios.get()으로 변경
-  const myFollowing = myFollowings.filter((myFollowing)=> myFollowing.userId==userId)[0].follow;
+export const checkMyFollowing = (state, {myFollowing}) => {
   state.myFollowing = myFollowing;
 };
 
-export const getUserRecipes = (state, {userId}) => {
-  //userRecipes get : axios.get()으로 변경
-  const userRecipes = recipes.filter((recipe) => recipe.userId==userId);  
+export const getUserRecipes = (state, {userRecipes}) => { 
   state.userRecipes = userRecipes;
 };
 
