fb71482e65123e130d5428972f33777bf72c31b0
Lee,YoungHoon <nobel6018@gmail.com>
2021-08-23 22:15:05 +0900

    (#114) 레시피 양방향 연관관계 설정, 코드 포맷팅, 변수명 조정

--- back-end-api/src/main/java/com/project/egloo/recipe/controller/RecipeController.java
+++ back-end-api/src/main/java/com/project/egloo/recipe/controller/RecipeController.java
@@ -4,7 +4,10 @@ import com.project.egloo.common.ResponseEntityObject;
 import com.project.egloo.common.exceptions.ErrorCode;
 import com.project.egloo.recipe.service.RecipeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -18,7 +21,7 @@ public class RecipeController {
 
 
     @GetMapping("/getRecipeByIngredients")
-    public ResponseEntityObject recipeByIngredients(@RequestParam List<String> ingredients){
-        return new ResponseEntityObject(ErrorCode.SUCCESS.getCode(), recipeService.getRecipeByIngredients(ingredients).toString(),"");
+    public ResponseEntityObject recipeByIngredients(@RequestParam List<String> ingredients) {
+        return new ResponseEntityObject(ErrorCode.SUCCESS.getCode(), recipeService.getRecipeByIngredients(ingredients).toString(), "");
     }
 }
--- back-end-api/src/main/java/com/project/egloo/recipe/domain/Recipe.java
+++ back-end-api/src/main/java/com/project/egloo/recipe/domain/Recipe.java
@@ -1,34 +1,38 @@
 package com.project.egloo.recipe.domain;
 
 import com.project.egloo.common.ColumnDescription;
-import lombok.*;
-import org.hibernate.annotations.DynamicInsert;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PUBLIC)
 @Getter
 @Setter
-@Data
-@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Recipe {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "recipe_id")
     @ColumnDescription("PK")
-    private Long    id;
+    private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
     @ColumnDescription("FK 카테고리 아이디")
     private Category category;
 
     @ColumnDescription("간략정보")
-    private String  summary;
+    private String summary;
 
     @ColumnDescription("레시피 이름")
-    private String  name;
+    private String name;
 
     @Enumerated(EnumType.STRING)
     @Column(columnDefinition = "varchar(255) default 'MIDDLE'")
@@ -36,23 +40,31 @@ public class Recipe {
     private Difficulty difficulty;
 
     @ColumnDescription("별점")
-    private Double  ratings;
-
-    @ColumnDescription("생성일자")
-    private Date    createTime;
-
-    @ColumnDescription("수정일자")
-    private Date    updateTime;
+    private Double ratings;
 
     @ColumnDescription("레시피 이미지")
-    private String  recipeImage;
+    private String image;
 
     @ColumnDescription("재료 총 가격")
-    private int     price;
+    private int price;
 
     @ColumnDescription("찜 수")
-    private int     countLike;
+    private int countLike;
 
     @ColumnDescription("리뷰 수")
-    private int     countReview;
+    private int countReview;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "recipe")
+    @ColumnDescription("요리 (읽기 전용, 양방향 연관관계 설정)")
+    private List<Cooking> cookings = new ArrayList<>();
+
+    @OneToMany(mappedBy = "recipe")
+    @ColumnDescription("요리 순서 (읽기 전용, 양방향 연관관계 설정)")
+    private List<RecipeProcess> recipeProcesses = new ArrayList<>();
+
+    @ColumnDescription("생성일자")
+    private OffsetDateTime createdAt;
+
+    @ColumnDescription("수정일자")
+    private OffsetDateTime updatedAt;
 }
