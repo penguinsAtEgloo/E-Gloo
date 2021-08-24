71493161bf4ebee7314b7962116dca68db819146
YoungHoon <nobel6018@gmail.com>
2021-08-24 18:54:51 +0900

    (#116) fix: token provider @Value works correctly now, formatting code, unit test for creating token

--- back-end-api/src/main/java/com/project/egloo/config/jwt/TokenProvider.java
+++ back-end-api/src/main/java/com/project/egloo/config/jwt/TokenProvider.java
@@ -1,14 +1,10 @@
 package com.project.egloo.config.jwt;
 
-import com.project.egloo.config.security.UserPrincipal;
 import com.project.egloo.member.domain.Member;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -26,19 +22,16 @@ import java.util.stream.Collectors;
 @Slf4j
 @Component
 public class TokenProvider implements InitializingBean {
+
     private static final String AUTHORITIES_KEY = "auth";
 
-    private final String secret;
-    private final long tokenValidityInMilliseconds;
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.token-validity-in-seconds}")
+    private Long tokenValidityInSeconds;
 
     private Key key;
-
-    public TokenProvider(
-            @Value("${jwt.secret}") String secret,
-            @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSeconds) {
-        this.secret = secret;
-        this.tokenValidityInMilliseconds = tokenValidityInSeconds * 1000;
-    }
 
     @Override
     public void afterPropertiesSet() {
@@ -48,32 +41,32 @@ public class TokenProvider implements InitializingBean {
 
     public String createToken(Authentication authentication) {
         String authorities = authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(","));
+            .map(GrantedAuthority::getAuthority)
+            .collect(Collectors.joining(","));
 
         long now = (new Date()).getTime();
-        Date validity = new Date(now + this.tokenValidityInMilliseconds);
+        Date validity = new Date(now + this.tokenValidityInSeconds);
 
         return Jwts.builder()
-                .setSubject(authentication.getName())
-                .claim(AUTHORITIES_KEY, authorities)
-                .signWith(key, SignatureAlgorithm.HS512)
-                .setExpiration(validity)
-                .compact();
+            .setSubject(authentication.getName())
+            .claim(AUTHORITIES_KEY, authorities)
+            .signWith(key, SignatureAlgorithm.HS512)
+            .setExpiration(validity)
+            .compact();
     }
 
     public Authentication getAuthentication(String token) {
         Claims claims = Jwts
-                .parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+            .parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
 
         Collection<? extends GrantedAuthority> authorities =
-                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
-                        .map(SimpleGrantedAuthority::new)
-                        .collect(Collectors.toList());
+            Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
 
         Member principal = new Member(claims.getSubject(), "", authorities);
 
---
+++ back-end-api/src/test/java/com/project/egloo/config/jwt/TokenProviderTest.java
@@ -0,0 +1,44 @@
+package com.project.egloo.config.jwt;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.Authentication;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class TokenProviderTest {
+
+    @Autowired
+    private TokenProvider tokenProvider;
+
+    @Test
+    public void createTokenTest() throws JSONException, IOException {
+        // given
+        Authentication authentication = new TestingAuthenticationToken("user-name", "secure-password-1234");
+
+        // when
+        String token = tokenProvider.createToken(authentication);
+
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] headerBytes = decoder.decode(token.split("\\.")[0]);
+        byte[] bodyBytes = decoder.decode(token.split("\\.")[1]);
+
+        JSONObject header = new JSONObject(new String(headerBytes));
+        JSONObject body = new JSONObject(new String(bodyBytes));
+
+        // then
+        assertThat(header.get("alg")).isEqualTo("HS512");
+
+        assertThat(body.get("sub")).isEqualTo("user-name");
+        assertThat(body.get("auth")).isEqualTo("");
+    }
+
+}
