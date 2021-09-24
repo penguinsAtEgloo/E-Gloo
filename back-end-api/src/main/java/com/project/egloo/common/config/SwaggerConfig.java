7aeb46bfbdfedfe55f33d847357717a7103f5cd3
Lee,YoungHoon <nobel6018@gmail.com>
2021-09-24 17:25:13 +0900

    feat: (#151) update swagger, spring boot

--- back-end-api/src/main/java/com/project/egloo/common/config/SwaggerConfig.java
+++ back-end-api/src/main/java/com/project/egloo/common/config/SwaggerConfig.java
@@ -1,53 +1,49 @@
 package com.project.egloo.common.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Collections;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
+
+    private final String authorization = "Authorization";
+
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-            .consumes(getConsumeContentTypes())
-            .produces(getProduceContentTypes())
-            .apiInfo(this.apiInfo())
-            .select()
-            .apis(RequestHandlerSelectors.basePackage("org.springframework.boot"))
-            .paths(PathSelectors.any())
-            .build();
+    public OpenAPI pointBerryOpenAPI() {
+
+        return new OpenAPI()
+            .info(
+                new Info()
+                    .title("Egloo API Document")
+                    .description("[Egloo] REST API")
+                    .contact(
+                        new Contact()
+                            .name("[Egloo Swagger]")
+                            .url("https://github.com/penguinsAtEgloo/E-Gloo")
+                            .email("helloegloo@gmail.com")
+                    )
+                    .version("v0.1.0"))
+            .components(
+                new Components()
+                    .addSecuritySchemes(authorization, bearerSecuritySchema())
+            )
+            .security(Collections.singletonList(new SecurityRequirement().addList(authorization)));
     }
 
-    private Set<String> getConsumeContentTypes() {
-        Set<String> consumes = new HashSet<>();
-        consumes.add("application/json;charset=UTF-8");
-        consumes.add("application/x-www-form-urlencoded");
-        return consumes;
-    }
-
-    private Set<String> getProduceContentTypes() {
-        Set<String> produces = new HashSet<>();
-        produces.add("application/json;charset=UTF-8");
-        return produces;
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-            .title("Egloo API Document")
-            .description("[Egloo] REST API")
-            .contact(new Contact("[Egloo Swagger]", "https://github.com/penguinsAtEgloo/E-Gloo", "helloegloo@gmail.com"))
-            .version("0.1.0")
-            .build();
+    private SecurityScheme bearerSecuritySchema() {
+        return new SecurityScheme()
+            .name(authorization)
+            .description("JWT Token")
+            .scheme("bearer")
+            .type(SecurityScheme.Type.HTTP)
+            .bearerFormat("JWT");
     }
 }
